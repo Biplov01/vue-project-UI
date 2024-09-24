635cd01b76fd0555541d9060d4bfb90e8d1441d6
Biplov Paneru <90428280+Biplov01@users.noreply.github.com>
2024-09-24 23:29:31 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+      orders: [
+        { id: '#5089', customer: 'Joseph Wheeler', date: '6 April, 2023', total: '$2,564', method: 'CC', status: 'Pending' },
+        { id: '#5088', customer: 'Joseph Wheeler', date: '6 April, 2023', total: '$2,564', method: 'CC', status: 'Pending' },
+        { id: '#5087', customer: 'Joseph Wheeler', date: '6 April, 2023', total: '$2,564', method: 'CC', status: 'Pending' },
+        { id: '#5086', customer: 'Joseph Wheeler', date: '6 April, 2023', total: '$2,564', method: 'CC', status: 'Pending' },
+        { id: '#5085', customer: 'Joseph Wheeler', date: '6 April, 2023', total: '$2,564', method: 'CC', status: 'Pending' },
+      ]
+    }
+  });
+  
